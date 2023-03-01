c6a41075852a31b16e37f96429c2e7b639272685
ChiaLingTW <babess501@gmail.com>
2023-03-01 23:50:42 +0800

    新增Vue資料動態顯示

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let appWeather = new Vue({
+    el: "#appWeather",
+    data: {
+        tag: "taichung_city",
+        place: "台中市",
+        low: 15,
+        high: 22,
+        rain: 45,
+        weather: "晴時多雲"
+    }
+})
